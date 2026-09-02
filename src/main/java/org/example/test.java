8961ea7a309a4b3cbea3cbac88b0c024dffd879d
JeffreyDarlington <63524406+jeffreydarlington@users.noreply.github.com>
2026-09-02 13:14:55 -0400

    Added test file java file

---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class test {
+        public static void main(String[] args) {
+            System.out.println("Java is working!");
+
+            int a = 5, b = 10;
+            System.out.println("Sum: " + (a + b));
+        }
+}
